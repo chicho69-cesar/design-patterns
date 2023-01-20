9091ee021e042a150ce348c89b4b4a5f82573441
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-20 15:29:11 -0600

    :sparkles: Prototype Shallow in C#

---
+++ csharp/PrototypeShallow/PrototypeShallow/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace PrototypeShallow {
+    public class Animal : ICloneable {
+        public string? Name { get; set; }
+        public int Feets { get; set; }
+
+        public object Clone() {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ csharp/PrototypeShallow/PrototypeShallow/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Prototype Shallow.- El patron prototype shallow o superficial, lo que busca es
+romper con la referencia de los objetos al momento de asignarlos de una variable 
+a otra, para esta hacemos uso del metodo Clone que forma parte de la interfaz
+ICloneable, el cual nos permite regresar una nueva instancia del objeto con los
+mismos datos pero otra referencia
+*/
+
+namespace PrototypeShallow {
+    class Program {
+        static void Main(string[] args) {
+            Animal animal1 = new() {
+                Name = "Oveja Dolly",
+                Feets = 4
+            };
+
+            Animal animal2 = animal1.Clone() as Animal ?? new();
+            animal2.Feets = 3;
+
+            Console.WriteLine(animal1.Feets);
+            Console.WriteLine(animal2.Feets);
+        }
+    }
+}
