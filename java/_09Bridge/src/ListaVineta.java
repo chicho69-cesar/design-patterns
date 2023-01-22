9049fd4b717be9baca6b22ff487d204ec8bc5d13
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-22 11:56:12 -0600

    :sparkles: Bridge in Java

---
+++ java/_09Bridge/src/ListaVineta.java
@@ -0,0 +1,16 @@
+public class ListaVineta extends ListasBase {
+    private char tipoVineta;
+
+    public void setTipoItem(char nuevoTipo) {
+        if (nuevoTipo > ' ') {
+            tipoVineta = nuevoTipo;
+        } else {
+            tipoVineta = '+';
+        }
+    }
+
+    @Override
+    public String obtenerItem(int index) {
+        return tipoVineta + " " + super.obtenerItem(index);
+    }
+}
